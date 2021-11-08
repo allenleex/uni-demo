7f439cd83f9d11aeeeb8b3ed4666d96be5225bf7
allenleex <allenleexxx@163.com>
2021-11-08 11:42:08 +0800

    chat

--- src/pages/team/index.vue
+++ src/pages/team/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-11-08 07:57:41
- * @LastEditTime: 2021-11-08 11:36:52
+ * @LastEditTime: 2021-11-08 11:41:20
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \uni-demo\src\pages\resource\index.vue
@@ -317,7 +317,7 @@ export default {
     detail: function () {
       console.log("------ chat");
       uni.navigateTo({
-        url: "./chat/chat",
+        url: "../chat/chat/chat",
       });
     },
   },
