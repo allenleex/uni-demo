45b4f7962ddb5f43832b89d7c1c51bbbcc899cb2
allenleex <allenleexxx@163.com>
2021-11-09 09:53:23 +0800

    team

--- src/pages/team/index.vue
+++ src/pages/team/index.vue
@@ -1,73 +1,71 @@
 <!--
  * @Author: your name
  * @Date: 2021-11-08 07:57:41
- * @LastEditTime: 2021-11-09 09:47:12
+ * @LastEditTime: 2021-11-09 09:52:49
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \uni-demo\src\pages\resource\index.vue
 -->
 <template>
-  <view class="container">
-    <view>
-      <tui-grid>
-        <block v-for="(item, index) in navlinks" :key="index">
-          <tui-grid-item :cell="4" @click="detail">
-            <navigator :url="item.url">
-              <text class="tui-grid-item-value" :wx-if="!item.icon">
-                {{ item.value }}
-              </text>
-              <view class="tui-grid-icon" :wx-if="item.icon">
-                <tui-icon
-                  :name="item.name"
-                  :size="30"
-                  :color="item.color"
-                ></tui-icon>
-              </view>
-              <text class="tui-grid-label">{{ item.title }}</text>
-              <!-- <view class="tui-rate-container">
-                <tui-rate :current="3" hollow="true" disabled="true"></tui-rate>
-              </view> -->
-            </navigator>
-          </tui-grid-item>
-        </block>
-      </tui-grid>
-      <view class="tui-title">
-        我创建的团队
-      </view>
-      <tui-card
-        v-for="(card, index) in teams"
-        :key="index"
-        :image="card.img"
-        :title="card.title"
-        :tag="card.tag"
-        @click="detail"
-      >
-        <!-- <template v-slot:body>
-          <view class="tui-default"> 卡片内容部分 slot=>body </view>
-        </template> -->
-        <!-- <template v-slot:footer>
-          <view class="tui-default"> 卡片底部 slot=>footer </view>
-        </template> -->
-      </tui-card>
-      <view class="tui-title">
-        已加入的团队
-      </view>
-      <tui-card
-        v-for="(card, index) in teams"
-        :key="index"
-        :image="card.img"
-        :title="card.title"
-        :tag="card.tag"
-        @click="detail"
-      >
-        <!-- <template v-slot:body>
-          <view class="tui-default"> 卡片内容部分 slot=>body </view>
-        </template> -->
-        <!-- <template v-slot:footer>
-          <view class="tui-default"> 卡片底部 slot=>footer </view>
-        </template> -->
-      </tui-card>
+  <view class="tui-container">
+    <tui-grid>
+      <block v-for="(item, index) in navlinks" :key="index">
+        <tui-grid-item :cell="4" @click="detail">
+          <navigator :url="item.url">
+            <text class="tui-grid-item-value" :wx-if="!item.icon">
+              {{ item.value }}
+            </text>
+            <view class="tui-grid-icon" :wx-if="item.icon">
+              <tui-icon
+                :name="item.name"
+                :size="30"
+                :color="item.color"
+              ></tui-icon>
+            </view>
+            <text class="tui-grid-label">{{ item.title }}</text>
+            <!-- <view class="tui-rate-container">
+              <tui-rate :current="3" hollow="true" disabled="true"></tui-rate>
+            </view> -->
+          </navigator>
+        </tui-grid-item>
+      </block>
+    </tui-grid>
+    <view class="tui-title">
+      我创建的团队
     </view>
+    <tui-card
+      v-for="(card, index) in teams"
+      :key="index"
+      :image="card.img"
+      :title="card.title"
+      :tag="card.tag"
+      @click="detail"
+    >
+      <!-- <template v-slot:body>
+        <view class="tui-default"> 卡片内容部分 slot=>body </view>
+      </template> -->
+      <!-- <template v-slot:footer>
+        <view class="tui-default"> 卡片底部 slot=>footer </view>
+      </template> -->
+    </tui-card>
+    <view class="tui-title">
+      已加入的团队
+    </view>
+    <tui-card
+      v-for="(card, index) in teams"
+      :key="index"
+      :image="card.img"
+      :title="card.title"
+      :tag="card.tag"
+      @click="detail"
+    >
+      <!-- <template v-slot:body>
+        <view class="tui-default"> 卡片内容部分 slot=>body </view>
+      </template> -->
+      <!-- <template v-slot:footer>
+        <view class="tui-default"> 卡片底部 slot=>footer </view>
+      </template> -->
+    </tui-card>
   </view>
 </template>
 
